f65afe07b4968f0352fd77c8fed691a37e1c3d04
Evgen <Evgen622@gmail.com>
2026-02-11 18:26:37 +0200

    solve jv-read-from-file task

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,47 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
+    public static final int FIRST_BOUNDARY = 0;
+    public static final int SECOND_BOUNDARY = 1;
+
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+
+        StringBuilder builder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String tempString = reader.readLine();
+            while (tempString != null) {
+                builder.append(tempString).append(" ");
+                tempString = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read data from file", e);
+        }
+
+        String[] filterdStrings = builder.toString().toLowerCase().split("\\W+");
+
+        builder = new StringBuilder();
+        for (String filteredString : filterdStrings) {
+            if (!filteredString.isEmpty()) {
+                String letter = filteredString.substring(FIRST_BOUNDARY, SECOND_BOUNDARY);
+                if (letter.equals("w")) {
+                    builder.append(filteredString).append(" ");
+                }
+            }
+        }
+
+        String[] sortingString = builder.toString().split(" ");
+
+        if (!sortingString[0].isEmpty()) {
+            Arrays.sort(sortingString);
+            return sortingString;
+        } else {
+            return new String[]{};
+        }
     }
 }
